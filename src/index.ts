bd24650c4d097f9adb25a8289126a556942b33bd
Maobugichi <immansamuel846@gmail.com>
2026-06-09 18:26:38 +0100

    add env for frontend url

--- src/index.ts
+++ src/index.ts
@@ -36,8 +36,8 @@ app.use(
 
 app.use(cors({
     origin: process.env.NODE_ENV === 'production' 
-    ? ["https://blessings-store-psi.vercel.app"]
-    : ["http://localhost:5173", "https://blessings-store-psi.vercel.app"],
+    ? process.env.FRONTEND_URL
+    : ["http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     maxAge: 86400
